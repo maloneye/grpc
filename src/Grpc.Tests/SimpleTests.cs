168fd0c95b3806d03c5a0945f811b38242dc6c2b
EM <maloneyewan@gmail.com>
2025-01-19 12:22:05 +0000

    still dont quite understand why rider has these as unversioned

---
+++ src/Grpc.Tests/SimpleTests.cs
@@ -0,0 +1,34 @@
+﻿using Grpc.Client;
+using Grpc.Server;
+using Shouldly;
+
+namespace Grpc.Tests;
+
+[TestClass]
+public sealed class SimpleTests
+{
+    [TestMethod("Given an known employee, then the service should correctly return there skills")]
+    public async Task T0()
+    { 
+        // Arrange
+        var employee = new Employee("Ewan");
+
+        var server = new Host();
+        var client = new Edge("http://localhost:5000");
+
+        try
+        {
+            await server.StartAsync(CancellationToken.None);
+
+            // Act
+            var skills = await client.RequestSkill(employee);
+
+            // Assert
+            skills.Count.ShouldBe(2);
+        }
+        finally
+        {
+            await server.StopAsync(CancellationToken.None);
+        }
+    }
+}
